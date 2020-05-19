8ceefa47f283d17720dd87261c4dbf317feb4403
cirith_gorgor666@hotmail.com <Binichtot1349>
2020-05-19 18:25:56 -0500

    Initial commit

---
+++ app/src/main/java/com/fgc/musicapp/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.fgc.musicapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.concurrent.thread
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        thread {
+            Thread.sleep((3 * 1000).toLong())
+            startMainActivity()
+        }.priority = Thread.NORM_PRIORITY
+    }
+
+    private fun startMainActivity() {
+        //val intent = Intent(applicationContext, ProfileUserActivity::class.java)
+        //startActivity(intent)
+        finish()
+    }
+}
